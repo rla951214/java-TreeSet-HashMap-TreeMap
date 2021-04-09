81ba1cef4f06659a946821fcd2624bc1a59f7cca
rla951214 <rla951214@naver.com>
2021-04-09 18:17:03 +0900

    'init'

---
+++ src/src/hashset/MemberHashSetTest.java
@@ -0,0 +1,27 @@
+package hashset;
+
+import collection.Member;
+
+public class MemberHashSetTest {
+
+	public static void main(String[] args) {
+		
+		MemberHashSet memberHashSet = new MemberHashSet();
+		
+		Member memberLee = new Member(201,"ÀÌ¼ø½Å");
+		Member memberKim = new Member(301,"±èÀ¯½Å");
+		Member memberShin = new Member(401,"½Å»çÀÓ´ç");
+		
+		memberHashSet.addMember(memberLee);
+		memberHashSet.addMember(memberKim);
+		memberHashSet.addMember(memberShin);
+		
+		memberHashSet.showAll();
+		
+		Member memberLee2 = new Member(201,"ÀÌ¸ù·æ");
+		memberHashSet.addMember(memberLee2);
+		
+		memberHashSet.showAll();
+	}
+
+}
---
+++ src/src/treeset/MemberTreeSetTest.java
@@ -0,0 +1,22 @@
+package treeset;
+
+import collection.Member;
+
+public class MemberTreeSetTest {
+
+	public static void main(String[] args) {
+		
+		MemberTreeSet memberHashSet = new MemberTreeSet();
+		
+		Member memberLee = new Member(102,"이순신");
+		Member memberKim = new Member(101,"김유신");
+		Member memberShin = new Member(103,"신사임당");
+		
+		memberHashSet.addMember(memberLee);
+		memberHashSet.addMember(memberKim);
+		memberHashSet.addMember(memberShin);
+		
+		memberHashSet.showAll();
+		
+	}
+}
